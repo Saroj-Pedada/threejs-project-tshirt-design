51300b694e5429e8f03db603f914db860128f479
Saroj-Pedada <sarojpedada@gmail.com>
2023-08-09 01:02:06 +0530

    fixed deployment

--- client/src/config/config.js
+++ client/src/config/config.js
@@ -1,6 +1,6 @@
 const config = {
   development: {
-    backendUrl: "https://tshirt-designer-66wf.onrender.com/api/v1/dalle",
+    backendUrl: "https://tshirt-designer-l6dy.onrender.com/api/v1/dalle",
   },
   production: {
     backendUrl: "https://devswag.onrender.com/api/v1/dalle",
